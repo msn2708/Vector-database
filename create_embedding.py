5b5169891f77d3cf748c72a383a92b41cca1c31b
Shahnawaz Mohammed <email.shahnawaz@gmail.com>
2023-09-22 08:39:20 -0700

    embedding vector to json format to store in DB

--- create_embedding.py
+++ create_embedding.py
@@ -1,8 +1,12 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 from get_config import Config
+import json
 
 MODEL = SentenceTransformer(Config().get_config().get('embedding_model'))
 
 def get_embedding(text):
-    return MODEL.encode(text)
+    embeddings = MODEL.encode(text)
+    return [json.dumps(embeddings.tolist()) for embedding in embeddings]
+
+    #return MODEL.encode(text).flatten()
