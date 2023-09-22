a50ecc3ca06bde03b6d133d5093fa9785ed6aa96
Shahnawaz Mohammed <email.shahnawaz@gmail.com>
2023-09-22 10:36:37 -0700

    modified get_embedding to return a string

--- create_embedding.py
+++ create_embedding.py
@@ -7,6 +7,4 @@ MODEL = SentenceTransformer(Config().get_config().get('embedding_model'))
 
 def get_embedding(text):
     embeddings = MODEL.encode(text)
-    return [json.dumps(embeddings.tolist()) for embedding in embeddings]
-
-    #return MODEL.encode(text).flatten()
+    return ''.join([json.dumps(embedding.tolist()) for embedding in embeddings])
