dbf2642ac38964eaeb1afc6640c433cc442af4b0
Shahnawaz Mohammed <email.shahnawaz@gmail.com>
2023-09-22 10:58:03 -0700

    debugging

--- list_files.py
+++ list_files.py
@@ -9,9 +9,11 @@ def list_files():
         data_dir = config.get('data_dir')     
         producer = Producer(config.get('producer'))
         for root,_,files in os.walk(data_dir):
-            for file in files: 
-                producer.produce(config.get('kafka-topic'), key=file, value=os.path.join(root, file))
-                producer.flush()
+            for file in files:
+                file = file.decode('utf-8')
+                print(file)
+                #producer.produce(config.get('kafka-topic'), key=file, value=os.path.join(root, file))
+                #producer.flush()
     except Exception as e: 
         print(f"Error in listing the directory: {e}")
     except KeyboardInterrupt:
